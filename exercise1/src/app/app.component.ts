c4fd966e383b077700fa767bb0f922a4665f8c8d
Ivy Ti <chivy1221@gmail.com>
2022-05-08 08:00:54 +0300

    added dashboard

--- exercise1/src/app/app.component.ts
+++ exercise1/src/app/app.component.ts
@@ -1,10 +1,120 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from "@angular/core";
+import { ChartConfiguration } from "chart.js";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'angular7';
+export class AppComponent implements AfterViewInit {
+  cs_chart: ChartConfiguration = {
+    type: "bar",
+    options: {
+      scales: {
+        y: {
+          ticks: {
+            callback: function (val) {
+              return val + "%";
+            },
+          },
+        },
+      },
+    },
+    data: {
+      labels: [
+        "Oct 1",
+        "Oct 2",
+        "Oct 3",
+        "Oct 4",
+        "Oct 5",
+        "Oct 6",
+        "Oct 7",
+        "Oct 8",
+        "Oct 9",
+        "Oct 10",
+        "Oct 11",
+        "Oct 12",
+      ],
+      datasets: [
+        {
+          label: "2020",
+          data: [25, 20, 30, 22, 17, 10, 18, 26, 28, 26, 20, 32],
+        },
+        {
+          label: "2019",
+          data: [15, 10, 20, 12, 7, 0, 8, 16, 18, 16, 10, 22],
+          backgroundColor: "#d2ddec",
+          hidden: true,
+        },
+      ],
+    },
+  };
+  tf_chart: ChartConfiguration = {
+    type: "doughnut",
+    options: {
+      plugins: {
+        tooltip: {
+          callbacks: {
+            afterLabel: function () {
+              return "%";
+            },
+          },
+        },
+      },
+    },
+    data: {
+      labels: ["Direct", "Organic", "Referral"],
+      datasets: [
+        {
+          data: [60, 25, 15],
+          backgroundColor: ["#2C7BE5", "#A6C5F7", "#D2DDEC"],
+        },
+        {
+          data: [15, 45, 20],
+          backgroundColor: ["#2C7BE5", "#A6C5F7", "#D2DDEC"],
+          hidden: true,
+        },
+      ],
+    },
+  };
+  spark_chart: ChartConfiguration = {
+    type: "line",
+    options: {
+      scales: {
+        y: {
+          display: false,
+        },
+        x: {
+          display: false,
+        },
+      },
+      elements: {
+        line: {
+          borderWidth: 2,
+        },
+        point: {
+          hoverRadius: 0,
+        },
+      },
+      plugins: {
+        tooltip: {
+          external: () => false,
+        },
+      },
+    },
+    data: {
+      labels: new Array(12).fill("Label"),
+      datasets: [
+        {
+          data: [0, 15, 10, 25, 30, 15, 40, 50, 80, 60, 55, 65],
+        },
+      ],
+    },
+  };
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+  }
 }
--- exercise1/src/app/app.module.ts
+++ exercise1/src/app/app.module.ts
@@ -1,18 +1,157 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule, Title } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NavComponent } from "./nav/nav.component";
+import { HeaderComponent } from "./header/header.component";
+import { NgChartsModule } from "ng2-charts";
+import {
+  Chart,
+  ArcElement,
+  BarController,
+  BarElement,
+  BubbleController,
+  CategoryScale,
+  Decimation,
+  DoughnutController,
+  Filler,
+  Legend,
+  LinearScale,
+  LineController,
+  LineElement,
+  LogarithmicScale,
+  PieController,
+  PointElement,
+  PolarAreaController,
+  RadarController,
+  RadialLinearScale,
+  ScatterController,
+  TimeScale,
+  TimeSeriesScale,
+  Tooltip,
+} from "chart.js";
+
+Chart.register(
+  ArcElement,
+  BarController,
+  BarElement,
+  BubbleController,
+  CategoryScale,
+  Decimation,
+  DoughnutController,
+  Filler,
+  Legend,
+  LinearScale,
+  LineController,
+  LineElement,
+  LogarithmicScale,
+  PieController,
+  PointElement,
+  PolarAreaController,
+  RadarController,
+  RadialLinearScale,
+  ScatterController,
+  TimeScale,
+  TimeSeriesScale,
+
+  Tooltip
+);
+
+function getCSSVariableValue(variable: string) {
+  return getComputedStyle(document.documentElement).getPropertyValue(variable);
+}
+
+const colors = {
+  gray: {
+    300: getCSSVariableValue("--bs-chart-gray-300"),
+    600: getCSSVariableValue("--bs-chart-gray-600"),
+    700: getCSSVariableValue("--bs-chart-gray-700"),
+    800: getCSSVariableValue("--bs-chart-gray-800"),
+    900: getCSSVariableValue("--bs-chart-gray-900"),
+  },
+  primary: {
+    100: getCSSVariableValue("--bs-chart-primary-100"),
+    300: getCSSVariableValue("--bs-chart-primary-300"),
+    700: getCSSVariableValue("--bs-chart-primary-700"),
+  },
+  black: getCSSVariableValue("--bs-dark"),
+  white: getCSSVariableValue("--bs-white"),
+  transparent: "transparent",
+};
+
+const fonts = {
+  base: "Cerebri Sans",
+};
+console.log(1);
+console.log(getCSSVariableValue("--bs-chart-primary-700"));
+
+Chart.defaults.responsive = true;
+Chart.defaults.maintainAspectRatio = false;
+
+// Default
+Chart.defaults.color = getCSSVariableValue("--bs-chart-default-color");
+Chart.defaults.font.family = fonts.base;
+Chart.defaults.font.size = 13;
+
+// Layout
+Chart.defaults.layout.padding = 0;
+
+// Legend
+Chart.defaults.plugins.legend.display = false;
+
+// Point
+Chart.defaults.elements.point.radius = 0;
+Chart.defaults.elements.point.backgroundColor = colors.primary[700];
+
+// Line
+Chart.defaults.elements.line.tension = 0.4;
+Chart.defaults.elements.line.borderWidth = 3;
+Chart.defaults.elements.line.borderColor = colors.primary[700];
+Chart.defaults.elements.line.backgroundColor = colors.transparent;
+Chart.defaults.elements.line.borderCapStyle = "round";
+
+// Bar
+Chart.defaults.elements.bar.backgroundColor = colors.primary[700];
+Chart.defaults.elements.bar.borderWidth = 0;
+Chart.defaults.elements.bar.borderRadius = 10;
+Chart.defaults.elements.bar.borderSkipped = false;
+Chart.defaults.datasets.bar.maxBarThickness = 10;
+
+// Arc
+Chart.defaults.elements.arc.backgroundColor = colors.primary[700];
+Chart.defaults.elements.arc.borderColor = getCSSVariableValue(
+  "--bs-chart-arc-border-color"
+);
+Chart.defaults.elements.arc.borderWidth = 4;
+Chart.defaults.elements.arc.hoverBorderColor = getCSSVariableValue(
+  "--bs-chart-arc-hover-border-color"
+);
+Chart.overrides.doughnut.cutout = "83%";
+Chart.overrides.doughnut.plugins.tooltip.callbacks.title = function (ctx) {
+  return ctx[0].label;
+};
+
+Chart.defaults.scales.linear.grid = {
+  ...Chart.defaults.scales.linear.grid,
+  borderDash: [2],
+  borderDashOffset: 2,
+  color: getCSSVariableValue("--bs-chart-grid-line-color"),
+  drawBorder: false,
+  drawTicks: false,
+};
+
+//  Chart.defaults.scales.linear.ticks.beginAtZero = true;
+Chart.defaults.scales.linear.ticks.padding = 10;
+Chart.defaults.scales.linear.ticks.stepSize = 10;
+
+Chart.defaults.scales.category.ticks.padding = 20;
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, NgChartsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
